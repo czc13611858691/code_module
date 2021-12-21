344d43e4ea6ed3976155ede3c080596af6d30280
czc13611858691 <61654383+czc13611858691@users.noreply.github.com>
2021-12-21 18:29:31 +0800

    Update hc595.c
    将gpioAPI注释掉

--- hc595/hc595.c
+++ hc595/hc595.c
@@ -1,12 +1,11 @@
 #include "hc595.h"
-#include "atmel_start.h"
 
-#define HC595_DS_SET LED_OUT_DAT_set_level(true)
-#define HC595_DS_RST LED_OUT_DAT_set_level(false)
-#define HC595_STCP_SET LED_CS_set_level(true)
-#define HC595_STCP_RST LED_CS_set_level(false)
-#define HC595_SHCP_SET LED_MCLK_set_level(true)
-#define HC595_SHCP_RST LED_MCLK_set_level(false)
+#define HC595_DS_SET //TODO: EXAMPLE--LED_OUT_DAT_set_level(true)
+#define HC595_DS_RST //TODO: EXAMPLE--LED_OUT_DAT_set_level(false)
+#define HC595_STCP_SET //TODO: EXAMPLE--LED_CS_set_level(true)
+#define HC595_STCP_RST //TODO: EXAMPLE--LED_CS_set_level(false)
+#define HC595_SHCP_SET //TODO: EXAMPLE--LED_MCLK_set_level(true)
+#define HC595_SHCP_RST //TODO: EXAMPLE--LED_MCLK_set_level(false)
 
 void HC595_Write(uint8_t *dataPtr, uint8_t icNum, uint8_t bitLen)
 {
